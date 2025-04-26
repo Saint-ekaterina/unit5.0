46e6925559509906a4c84c5f52d582dcc19ba8ab
Ekaterina <katena.ponomareva.01@inbox.ru>
2025-04-27 00:12:11 +0700

    fixed it

--- src/test/java/ru/netology/delivery/data/DataGenerator.java
+++ src/test/java/ru/netology/delivery/data/DataGenerator.java
@@ -1,52 +1,74 @@
 package ru.netology.delivery.data;
 
 import com.github.javafaker.Faker;
-import lombok.Value;
-
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
 import java.util.Locale;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class DataGenerator {
-    private DataGenerator() {
-    }
+    private DataGenerator() {}
 
-    public static String generateDate(int shift) {
-        return LocalDate.now().plusDays(shift).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    private static final List<String> CITIES = List.of(
+            "Москва", "Санкт-Петербург", "Новосибирск",
+            "Екатеринбург", "Казань", "Нижний Новгород",
+            "Челябинск", "Самара", "Омск", "Ростов-на-Дону"
+    );
+
+    public static String generateDate(int daysToAdd) {
+        return LocalDate.now()
+                .plusDays(daysToAdd)
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
     public static String generateCity() {
-        var city = new String[] {
-                "Пермь", "Ижевск", "Екатеринбург", "Казань", "Оренбург",
-                "Самара", "Тольятти", "Новосибирск", "Киров", "Тюмень"
-        };
-        return city[new Random().nextInt(city.length)];
+        return CITIES.get(ThreadLocalRandom.current().nextInt(CITIES.size()));
     }
 
     public static String generateName(String locale) {
-        var faker = new Faker(new Locale(locale));
+        Faker faker = new Faker(new Locale(locale));
         return faker.name().lastName() + " " + faker.name().firstName();
     }
 
     public static String generatePhone(String locale) {
-        var faker = new Faker(new Locale(locale));
+        Faker faker = new Faker(new Locale(locale));
         return faker.phoneNumber().phoneNumber();
     }
 
     public static class Registration {
-        private Registration() {
-        }
+        private Registration() {}
 
         public static UserInfo generateUser(String locale) {
-            return new UserInfo(generateCity(), generateName(locale), generatePhone(locale));
+            return new UserInfo(
+                    generateCity(),
+                    generateName(locale),
+                    generatePhone(locale)
+            );
         }
     }
 
-    @Value
     public static class UserInfo {
-        String city;
-        String name;
-        String phone;
+        private final String city;
+        private final String name;
+        private final String phone;
+
+        public UserInfo(String city, String name, String phone) {
+            this.city = city;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
     }
 }
--- src/test/java/ru/netology/delivery/test/DeliveryTest.java
+++ src/test/java/ru/netology/delivery/test/DeliveryTest.java
@@ -1,8 +1,6 @@
 package ru.netology.delivery.test;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.openqa.selenium.Keys;
 import ru.netology.delivery.data.DataGenerator;
 
@@ -10,41 +8,89 @@ import java.time.Duration;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
+import static ru.netology.delivery.data.DataGenerator.*;
 
 class DeliveryTest {
 
     @BeforeEach
-    void setup() {
+    void openBrowser() {
         open("http://localhost:9999");
     }
 
     @Test
-    @DisplayName("Should successful plan and replan meeting")
-    void shouldSuccessfulPlanAndReplanMeeting() {
+    @DisplayName("Пользователь может запланировать и перенести встречу")
+    void shouldScheduleAndRescheduleMeeting() {
+        // Подготовка тестовых данных
         var validUser = DataGenerator.Registration.generateUser("ru");
-        var daysToAddForFirstMeeting = 4;
-        var firstMeetingDate = DataGenerator.generateDate(daysToAddForFirstMeeting);
-        var daysToAddForSecondMeeting = 7;
-        var secondMeetingDate = DataGenerator.generateDate(daysToAddForSecondMeeting);
-        $("[data-test-id='city'] input").setValue(validUser.getCity());
+        var firstMeetingDay = 4; // дней до первой встречи
+        var secondMeetingDay = 7; // дней до второй встречи
+
+        var firstDate = DataGenerator.generateDate(firstMeetingDay);
+        var secondDate = DataGenerator.generateDate(secondMeetingDay);
+
+        // Заполняем форму для первой встречи
+        enterCity(validUser.getCity());
+        enterDate(firstDate);
+        enterName(validUser.getName());
+        enterPhone(validUser.getPhone());
+        agreeWithTerms();
+        clickScheduleButton();
+
+        // Проверяем подтверждение первой встречи
+        checkSuccessNotification("Встреча успешно запланирована на " + firstDate);
+
+        // Меняем дату на вторую
+        enterDate(secondDate);
+        clickScheduleButton();
+
+        // Подтверждаем перенос встречи
+        checkRescheduleConfirmation();
+        clickRescheduleButton();
+
+        // Проверяем подтверждение второй встречи
+        checkSuccessNotification("Встреча успешно запланирована на " + secondDate);
+    }
+
+    // Вспомогательные методы для лучшей читаемости
+    private void enterCity(String city) {
+        $("[data-test-id='city'] input").setValue(city);
+    }
+
+    private void enterDate(String date) {
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(firstMeetingDate);
-        $("[data-test-id='name'] input").setValue(validUser.getName());
-        $("[data-test-id='phone'] input").setValue(validUser.getPhone());
+        $("[data-test-id='date'] input").setValue(date);
+    }
+
+    private void enterName(String name) {
+        $("[data-test-id='name'] input").setValue(name);
+    }
+
+    private void enterPhone(String phone) {
+        $("[data-test-id='phone'] input").setValue(phone);
+    }
+
+    private void agreeWithTerms() {
         $("[data-test-id='agreement']").click();
+    }
+
+    private void clickScheduleButton() {
         $(byText("Запланировать")).click();
-        $(byText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
-        $("[data-test-id='success-notification'].notification .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + firstMeetingDate)).shouldBe(visible);
-        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(secondMeetingDate);
-        $(byText("Запланировать")).click();
-        $("[data-test-id='replan-notification'].notification .notification__content")
-                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?")).shouldBe(visible);
+    }
+
+    private void clickRescheduleButton() {
         $(byText("Перепланировать")).click();
-        $("[data-test-id='success-notification'].notification .notification__content")
-                .shouldHave(text("Встреча успешно запланирована на " + secondMeetingDate)).shouldBe(visible);
+    }
+
+    private void checkSuccessNotification(String expectedText) {
+        $("[data-test-id='success-notification']")
+                .shouldBe(visible, Duration.ofSeconds(15))
+                .shouldHave(text(expectedText));
+    }
+
+    private void checkRescheduleConfirmation() {
+        $("[data-test-id='replan-notification']")
+                .shouldBe(visible)
+                .shouldHave(text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
     }
 }
